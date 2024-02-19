9940e15dff6b916b3634640f7a121516e06f487b
JoshUptons <itsjoshupton@gmail.com>
2024-02-19 17:15:46 -0500

    fixed the search and insert algo

--- python/searchInsertPosition.py
+++ python/searchInsertPosition.py
@@ -1,38 +1,27 @@
 from typing import List
 
 
-def bs(list: List[int], high: int, low: int, target: int) -> int:
-    m = (high + low) // 2
-    if m == 0 and list[0] != target:
-        return 0
-    if m == len(list) - 1 and list[len(list) - 1] != target:
-        return len(list)
-    if target == list[m]:
+def bs(nums: List[int], high: int, low: int, target: int) -> int:
+    m = (high - low) // 2 + low
+    if nums[m] == target:
+        while nums[m - 1] == target:
+            m -= 1
         return m
-    elif target < list[m]:
-        if target > list[m - 1]:
+    if target < nums[m]:
+        high = m
+        if target > nums[m - 1] or m == 0:
             return m
-        elif target == list[m - 1]:
-            return m - 1
-        else:
-            return bs(list, m, 0, target)
-    else:
-        if target < list[m + 1] or target == list[m + 1]:
+    if target > nums[m]:
+        low = m
+        if (m < len(nums) - 1
+                and target < nums[m + 1]) or (m == len(nums) - 1
+                                              and target != nums[m]):
             return m + 1
-        else:
-            return bs(list, len(list), m, target)
+
+    return bs(nums, high, low, target)
 
 
-def searchInsert(nums: List[int], target: int) -> int:
-    if len(nums) == 0:
-        return 0
+nums = [1, 3, 5, 7, 12, 15]
+result = bs(nums, len(nums), 0, 16)
 
-    if len(nums) == 1:
-        return (1, 0)[nums[0] > target]
-
-    return bs(nums, len(nums), 0, target)
-
-
-l = [1, 3]
-target = 1
-print(searchInsert(l, target))
+print(result)
